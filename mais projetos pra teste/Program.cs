a2105477366160a8e2c27dddb39cb01b30824c30
DanielSaldanha <danielpedrosaldanha@gmail.com>
2024-06-25 16:45:26 -0300

    Adicionar arquivos de projeto.

---
+++ mais projetos pra teste/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mais_projetos_pra_teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //LIST
+            //LISTA PRA CONTROLAR VALORES OU OBJETOS
+            List<string> List = new List<string>();//nomes datetimes idade e por ai vai porra
+            List.Add("cu");
+            Console.WriteLine(List[0]);
+            List.Add("penis");
+            Console.WriteLine(List[1]);
+            List.Add("tetas");
+            Console.WriteLine(List[2]);
+            List.Add("cu");
+            List.Add("cu");
+            List.Add("cu");
+            List.Add("cu");
+            List.Add("cu");
+            List.Add("cu");
+            List.Add("cu");
+            List.Add("cu");
+            List.Add("cu");
+            List.Add("cu");
+
+            foreach (var item in List)
+            {
+                Console.WriteLine(item);
+            }
+            //GET E SET COMTROLA A ENTRADA E SAIDA DE VALORES
+            */
+
+            //CALCULADORA
+            /*
+            Console.WriteLine("faleme uma conta");
+            Console.WriteLine("1-adiççao");
+            Console.WriteLine("2-subtraçao");
+            Console.WriteLine("3-multiplicaçao");
+            Console.WriteLine("4-divisao");
+
+            int operador = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digita um numero");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("outor pfv");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+
+            switch (operador)
+            {
+                case 1:
+                    {
+                        resultado = addiction(num1, num2);
+                        break;
+                    }
+                case 2:
+                    {
+                        resultado = addiction2(num1, num2);
+                        break;
+                    }
+                case 3:
+                    {
+                        resultado = addiction3(num1, num2);
+                        break;
+                    }
+                case 4:
+                    {
+                        resultado = addiction4(num1, num2);
+                        break;
+                    }
+                default:
+                    Console.WriteLine("impossivel fazer tal calculo");
+                    break;
+
+            }
+
+            Console.WriteLine("o resultado de " + num1 + " e " + num2 + " e " + resultado);
+            */
+
+            
+            Console.ReadLine();
+
+        }
+        /*
+        public static int addiction(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+        public static int addiction2(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            return resultado;
+        }
+        public static int addiction3(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            return resultado;
+        }
+        public static int addiction4(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            return resultado;
+        }
+       */
+        
+    }
+}
---
+++ mais projetos pra teste/carrinhoiteins.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mais_projetos_pra_teste
+{
+    internal class carrinhoiteins
+    {
+
+    }
+}
